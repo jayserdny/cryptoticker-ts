7f9ae2b2688194c9bcee10ac7710b2aad765b654
Jayser Mendez <jayser.mendez@stu.bcc.cuny.edu>
2018-01-20 05:44:59 -0500

    request library added, functions modified, and Price interface created

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,15 @@
-declare function require(name:string);
+import * as request from 'request';
 const cryptoValues = require("./crypto.json");
-const coinMarketCapUrl = "http://api.coinmarketcap.com/v1/ticker/";
+const coinMarketCapUrl = "https://api.coinmarketcap.com/v1/ticker/";
 const worldCoinIndexURL = "https://www.worldcoinindex.com/apiservice/json?key=";
-
-/**
- * @method getURL: fetch an api and return its body
- * @param {string} url: string with the url
- * @returns Promise with the http response
- */
-const getURL = function(url: string) {
-    // return new pending promise
-    return new Promise((resolve, reject) => {
-      // select http or https module, depending on reqested url
-      const lib = url.startsWith('https') ? require('https') : require('http');
-      const request = lib.get(url, (response: any) => {
-        // handle http errors
-        if (response.statusCode < 200 || response.statusCode > 299) {
-           reject(new Error('Failed to load page, status code: ' + response.statusCode));
-        }
-        // temporary data holder
-        const body: Array<any> = [];
-        // on every content chunk, push it to the data array
-        response.on('data', (chunk: any) => body.push(chunk));
-        // we are done, resolve promise with those joined chunks
-        response.on('end', () => resolve(body.join('')));
-      });
-      // handle connection errors of the request
-      request.on('error', (err: Error) => reject(err));
-    }).catch((err: Error) => {
-        throw err;
-    });
-}
 
 /**
  * @method getCrypto: Return the crypto value from the JSON
  * @param {string} crypto: crypto to query
  * @returns string with the crypto
  */
-const getCrypto = function(crypto: string): string {
-    return cryptoValues[crypto.toLowerCase()] || crypto.toLocaleLowerCase();
+const getCrypto = function(crypto: string) {
+    return cryptoValues[crypto.toLowerCase()] || crypto.toLowerCase();
 }
 
 /**
@@ -46,18 +17,63 @@ const getCrypto = function(crypto: string): string {
  * @param {string} crypto: crypto to query
  */
 export function price(crypto: string): Promise<any> {
-
     return new Promise((resolve, reject) => {
         if (!crypto) reject(new Error('Coin Not Found'));
 
         else {
-            const requestUrl = coinMarketCapUrl + "/" + getCrypto(crypto);
-
-            getURL(requestUrl).then((data: any) => {
-                if (data) resolve(JSON.parse(data));
-
+            let url = coinMarketCapUrl + getCrypto(crypto);
+            request(url, (error, res, body) => {
+                if (!error && res.statusCode === 200) resolve(JSON.parse(body));
                 else reject(new Error('Coin Not Found'));
             });
         }
     });
+}
+
+export function priceWCI(crypto, key): Promise<any> {
+    return new Promise((resolve, reject) => {
+        if (!crypto) reject(new Error('Coin Not Found'));
+        else if (!key) reject(new Error('Key Not Provided'));
+        
+        else {
+            const cryptoId = getCrypto(crypto);
+            const requestUrl = worldCoinIndexURL + key;
+            request(requestUrl, (error, res, body) => {
+                if (!error && res.statusCode === 200) {
+                    const response = JSON.parse(body);
+                    var filteredJson = response.Markets.filter((cryptoValue) => {
+                        return cryptoValue.Name.toLowerCase() === cryptoId;
+                    });
+                    resolve(filteredJson);
+                }
+                else reject(new Error('Coin Not Found'));
+            });
+        }
+    });
+}
+
+price('btc').then(data => {
+    console.log(data);
+})
+
+/**
+ * INTERFACES
+ */
+
+export interface Price {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: string;
+    price_usd: string;
+    '24h_volume_usd': string;
+    market_cap_usd: string;
+    available_supply: string;
+    total_supply: string;
+    max_supply: string;
+    percent_change_1h: string;
+    percent_change_24h: string;
+    percent_change_7d: string;
+    last_updated: string;
+
 }
